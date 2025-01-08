1528fef42f23113a9230ce29b0caadde70e706c3
biheng <rayer4u@gmail.com>
2025-01-08 20:27:26 +0800

    feat: ddns支持多子域名更新

--- omv/ddns/README.md
+++ omv/ddns/README.md
@@ -1,7 +1,16 @@
 # ddns
 
 dnspod支持。参照了[dnspod-shell](https://github.com/anrip/dnspod-shell)和[dnspod-ddns](https://github.com/strahe/dnspod-ddns)的结构并使用了其中的get_ip.py。重写目的是减少请求数，并去掉了自行调度，调度采用crontab
+支持多子域名,具体详见`.env.sample`
 
 ```shell
+# 依赖
 pip3 install dnspod-python python-dotenv
+
+# 定时调用，以下每5分钟
+crontab -e
+*/5 * * * * python3 /root/omv/ddns/ddns.py >> /root/omv/ddns/ddns.log 2>&1
+
+# 查看更新日志
+cat ddns.log  | grep -B 5 Updated
 ```
--- omv/ddns/ddns.py
+++ omv/ddns/ddns.py
@@ -12,29 +12,38 @@ load_dotenv()
 def main():
     login_token = os.getenv('LOGIN_TOKEN')
     domain = os.getenv('DOMAIN')
-    sub_domain = os.getenv('SUB_DOMAIN')
+    # 去掉多余的''
+    sub_domains = [sub for sub in os.getenv('SUB_DOMAIN').split(',') if sub]
 
-    print("Updating Domain: %s.%s at %s" % (sub_domain, domain, datetime.datetime.now().isoformat()))
+    print("Checking Domain: %s at %s" % (domain, datetime.datetime.now().isoformat()))
     current_ip = get_ip()
-    print("Host Ip: %s" % current_ip)
+    print("Host IP: %s" % current_ip)
 
     api = apicn.DomainInfo(domain_id='', domain=domain, login_token=login_token)
     domain_id = api().get("domain", {}).get("id")
 
-    api = apicn.RecordList(domain_id, sub_domain=sub_domain, record_type='A', login_token=login_token)
-    record = api().get('records')[0]
-    record_ip = record.get('value')
-    record_id = record.get('id')
-    record_line = record.get('line')
-    print("Last Ip: %s" % record_ip)
-    
-    if current_ip != record_ip:
-        api = apicn.RecordDdns(record_id, sub_domain, record_line, domain_id=domain_id, value=current_ip, login_token=login_token)
-        ret = api()
-        new_ip = ret.get('record').get('value')
-        print('Updated: %s at %s' % (new_ip, datetime.datetime.now().isoformat()))
-    else:
-        print('Last IP is the same as current IP!')
+    rl_api = apicn.RecordList(domain_id, record_type='A', login_token=login_token)
+    records = rl_api().get('records')
+    updated = False
+    for record in records:
+        if sub_domains and record['name'] not in sub_domains:
+            print("Domain: %s.%s Skiped" % (record['name'], domain))
+            continue
+        sub_domain = record['name']
+        record_ip = record.get('value')
+        record_id = record.get('id')
+        record_line = record.get('line')
+
+        if current_ip != record_ip:
+            api = apicn.RecordDdns(record_id, sub_domain, record_line, domain_id=domain_id, value=current_ip, login_token=login_token)
+            ret = api()
+            new_ip = ret.get('record').get('value')
+            print("Domain: %s.%s IP %s changed to %s at %s!" % (sub_domain, domain, record_ip, new_ip, datetime.datetime.now().isoformat()))
+            updated = True
+        else:
+            print("Domain: %s.%s IP reserved." % (sub_domain, domain))
+    if updated:
+        print('Updated at %s!' % (datetime.datetime.now().isoformat()))
 
 
 if __name__ == '__main__':
