9cf3b35cc632459b8cdb39c38687e46c35abe508
biheng <rayer4u@gmail.com>
2025-02-16 15:12:18 +0800

    feat: 完善letsencrypt配置，方便支持其他域名

--- omv/letsencrypt/renew.sh
+++ omv/letsencrypt/renew.sh
@@ -3,5 +3,11 @@ base_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 cd "$base_path"
 source .env
-docker-compose run -u$(id -u):$(id -g) --rm renew
-python3 cert_change_update.py ${HOST_CERT_DIR}/live/${DOMAIN}/fullchain.pem
+id=$(docker compose run -d  -u$(id -u):$(id -g) renew)
+ret=$(docker wait $id)
+if [ $ret == 0 ]
+then
+  # 成功则进行证书更新检查和处理服务，并删除相应已完成容器
+  python3 cert_change_update.py ${HOST_CERT_DIR}/live/${DOMAIN}/fullchain.pem
+  docker rm $id
+fi
